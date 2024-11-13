6629d3a2dd64bececbb967bc4e7f988686780630
Pedro Lima <ped7864@gmail.com>
2024-11-13 13:20:50 -0300

    Implementando Content Negotiation via Query Parameter

---
+++ rest-with-spring-boot-and-java-erundio/src/main/java/com/erudio/rest_with_spring_boot_and_java_erundio/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.erudio.rest_with_spring_boot_and_java_erundio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        //Via QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
+
+        configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+}
